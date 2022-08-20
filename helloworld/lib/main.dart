f5973d90c2afa02114b1651a2016b09b9fb696e2
Salih Alcı <salihalci@gmail.com>
2022-08-20 18:48:41 +0300

    button actions added

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
- Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello!'),) ;
+class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print("Answer choosen!");
+  }
+
+  Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite animal?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(children: [
+          Text("The question!"),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text('Answer 3'),
+          ),
+        ]),
+      ),
+    );
   }
 }
